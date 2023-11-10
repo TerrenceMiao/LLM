048731c0d964fc658af755b5221c20467a65d316
Terrence Miao <terrence.miao@gmail.com>
2023-11-11 00:40:49 +1100

    Update user's avatar

--- chatgpt-retrieval/htmlTemplates.py
+++ chatgpt-retrieval/htmlTemplates.py
@@ -37,7 +37,7 @@ bot_template = """
 user_template = """
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
+        <img src="https://i.ibb.co/wRtZstJ/Aurora.png">
     </div>
     <div class="message">{{MSG}}</div>
 </div>

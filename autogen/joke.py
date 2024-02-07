27209e369e61a54203bbb5bf79f2e1968d38b9b8
Terrence Miao <terrence.miao@gmail.com>
2024-02-07 13:03:55 +1100

    Add a demo how AutoGen "Tell a joke" with local LLM

---
+++ autogen/joke.py
@@ -0,0 +1,57 @@
+'''
+"Tell a joke" by using AutoGen with local LLM - TheBloke/Llama-2-7B-Chat-GGUF, llama-2-7b-chat.Q4_K_M.gguf. To run:
+
+$ rm -rf .cache
+$ python joke.py
+'''
+
+import autogen
+
+llm_config = {"config_list": [{
+    "model": "gpt-3.5-turbo-mm",
+}]}
+
+llm_config_local = {"config_list": [{
+    "model": "llama2",
+    "base_url": "http://localhost:1234/v1",
+}]}
+
+bob = autogen.AssistantAgent(
+    name = "Bob",
+    system_message = "You love telling jokes",
+    llm_config = llm_config_local,
+)
+
+alice = autogen.AssistantAgent (
+    name = "Alice",
+    system_message = "Criticise the joke. After criticising is done, don't ask more jokes, just reply 'TERMINATE'",
+    llm_config = llm_config_local,
+)
+
+def termination_message(msg) :
+    return "TERMINATE" in str(msg.get("content", ""))
+
+user_proxy = autogen.UserProxyAgent(
+    name = "user_proxy",
+    code_execution_config = {"use_docker": False},
+    is_termination_msg = termination_message,
+    human_input_mode = "NEVER",
+)
+
+groupchat = autogen.GroupChat(
+    agents = [bob, alice, user_proxy],
+    messages = [],
+    speaker_selection_method = "round_robin",
+)
+
+manager = autogen.GroupChatManager(
+    groupchat = groupchat,
+    code_execution_config = {"use_docker": False},
+    llm_config = llm_config_local,
+    is_termination_msg = termination_message,
+)
+
+user_proxy.initiate_chat(
+    manager,
+    message = "Tell a joke",
+)

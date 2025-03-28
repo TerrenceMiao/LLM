eb1dfe96d02237434f492a89f89756afb0498cc7
Terrence Miao <terrence.miao@gmail.com>
2025-03-28 15:12:00 +1100

    Add Browser Use example

---
+++ browser-use/agent.py
@@ -0,0 +1,26 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from browser_use import Agent
+from pydantic import SecretStr
+
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+import asyncio
+
+# Initialize the model
+llm = ChatGoogleGenerativeAI(model='gemini-2.5-pro-exp-03-25', api_key=SecretStr(os.getenv('GEMINI_API_KEY')))
+
+# Create agent with the model
+async def main():
+    agent = Agent(
+        task="Compare the price of gpt-4o and DeepSeek-V3",
+        llm=llm,
+    )
+    result = await agent.run()
+    print(result)
+
+asyncio.run(main())
